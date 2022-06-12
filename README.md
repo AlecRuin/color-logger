155165bbfa8682720344f3f73f963d607cf1b5e6
Alec Harvey <ruinhard@gmail.com>
2022-06-12 18:44:20 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,54 @@
+
+  ![badge](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)
+  ![badge](https://img.shields.io/github/downloads/AlecRuin/color-logger/total?style=for-the-badge)
+  ![Badge](https://img.shields.io/github/languages/code-size/AlecRuin/color-logger?style=for-the-badge)
+  ![Badge](https://img.shields.io/github/issues/AlecRuin/color-logger?style=for-the-badge)
+
+  ![Badge](https://img.shields.io/github/stars/AlecRuin/color-logger?style=social)
+### Table of Contents 
+
+[Description](#Description)
+
+[Install instructions](#Install-instructions)
+
+[Usage](#Use-instructions)
+
+[Credits](#Credits)
+
+[License](#License)
+
+[How to contribute](#How-to-contribute)
+# Color Logger
+
+## Description
+
+This package uses colors, emojis, severity, and context to allow the user to easily log everything to the console
+
+## Install instructions
+
+Simply add to package.json file or install through the command line.
+```sh
+    npm install @alecruin/color-logger@1.0.0
+```
+or
+```json
+"dependencies": {
+    "@AlecRuin/color-logger":"1.0.0"
+  }
+```
+
+## Use instructions
+
+Once you require the project, simply call the print statement and pass through the context, severity, and message 
+
+(Context meaning server/client. NOT THE LINE FROM WHICH THE CALLWAS MADE)
+
+
+## License 
+
+This project uses the Apache License 2.0 license
+
+[License file](LICENSE.txt)
+## How to contribute
+
+Just make a well-documented pull request, and I'll hopefully get around to looking at it!
--- index.js
+++ index.js
@@ -1,3 +1,59 @@
-module.exports= ()=>{
-    console.log("Hellow world! this is inside the github package registry!")
+const settings= require("./settings.json")
+console.log(settings.showContext);
+/**
+ * console.logs very easy-to-read messages to the console. 
+ * 
+ * @param {String} msg The message body
+ * @param {Error} error You must pass through a newly created Error to log the line from which the call was made
+ * @param {Number} [severity] A number ranging in 1-5 on how serious the bug may be. Defaults to 1
+ * @param {Boolean} [isClient] A boolean value on if the log was called on the client side, or server side. Defaults to client
+ */
+module.exports.print=(msg,error,severity=1,isClient=true)=>{
+    if (settings.toUpper) msg.toUpperCase()
+    var Context=""
+    var SeverityMsg=""
+    if (settings.showContext){
+        switch (isClient){
+            case false:
+                (settings.useEmojis) ? Context="[🖥️]" : Context="[SERVER]"
+                break;
+            default:
+                (settings.useEmojis) ? Context="[👨‍💻]" : Context="[CLIENT]";
+                break;
+        }  
+    }
+    var stack = error.stack.toString().split(/\r\n|\n/)[1].split(" ");
+    stack=stack[stack.length-1]
+    var Origin=`[${stack.slice(1,stack.length-1).slice(stack.lastIndexOf("\\"),stack.length)}]`
+    if (msg === '') {
+        msg = '"👨‍💻"';
+    }
+    console.log(`${Context} ${SeverityMsg} ${Origin}`);  
 }
+/*
+🖥️
+function Module.Print(Severity,Origin,Message)
+	local SeverityMsg
+	if Severity ==0 then
+		SeverityMsg=" [ LOG/📜 ] "
+	elseif Severity ==1 then
+		SeverityMsg=" [ NONE/❔ ] "
+	elseif Severity ==2 then
+		SeverityMsg=" [ LOW/⚠️ ] "
+	elseif Severity ==3 then
+		SeverityMsg=" [ MINOR/❗ ] "
+	elseif Severity ==4 then
+		SeverityMsg=" [ MAJOR/❗❗ ] "
+	elseif Severity ==5 then
+		SeverityMsg=" [ CRITICAL/❗❗❗ ] "
+	else
+		SeverityMsg=" [ NONE/❔ ] "
+	end
+	if typeof(Message)==table then
+		return "[👨‍💻]"..SeverityMsg.."[ " ..Origin.." ]: ",Message
+	else
+		Message=string.upper(Message)
+		return "[👨‍💻]"..SeverityMsg.."[ " ..Origin.." ]: "..Message
+	end
+end
+*/
