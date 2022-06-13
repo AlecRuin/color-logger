b73a0c6097ca1f8ef7bcaf1f384a840279d0c6f7
Alec Harvey <ruinhard@gmail.com>
2022-06-13 12:19:11 -0500

    removed testing scripts and changed version

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const settings= require("./settings.json")
+const settings= JSON.parse(JSON.stringify(require("./settings.json")))
 /**
  * console.logs very easy-to-read messages to the console. 
  * 
  * @param {String} msg The message body
  * @param {Error} error You must pass through a newly created Error to log the line from which the call was made
- * @param {Number} [severity] A number ranging in 1-5 on how serious the bug may be. Defaults to 1
+ * @param {Number} [severity] A number ranging in 1-5 on how serious the bug may be. Defaults to 0
  * @param {Boolean} [isClient] A boolean value on if the log was called on the client side, or server side. Defaults to client
  */
-module.exports.print=(msg,error,severity=1,isClient=true)=>{
+module.exports.print=(msg,error,options={isClient:true,severity:0})=>{
     if (settings.toUpper) msg.toUpperCase()
     var Context=""
     var SeverityMsg=""
     if (settings.showContext){
-        switch (isClient){
+        switch (options.isClient){
             case false:
                 (settings.useEmojis) ? Context="[🖥️]" : Context="\x1b[34m[SERVER]"
                 break;
@@ -21,24 +21,24 @@ module.exports.print=(msg,error,severity=1,isClient=true)=>{
                 break;
         }  
     }
-    switch (severity){
+    switch (options.severity){
         case 1:
-            SeverityMsg=`[NONE${(settings.useEmojis) ? "/❔" :""}]`
+            SeverityMsg=`\x1b[35m[${settings[1].keyword}${(settings.useEmojis) ? "/❔" :""}]`
             break;
         case 2:
-            SeverityMsg=`[LOW${(settings.useEmojis) ? "/⚠️" :""}]`
+            SeverityMsg=`\x1b[33m[${settings[2].keyword}${(settings.useEmojis) ? "/⚠️" :""}]`
             break;
         case 3:
-            SeverityMsg=`[MINOR${(settings.useEmojis) ? "/❗" :""}]`
+            SeverityMsg=`\x1b[33m[${settings[3].keyword}${(settings.useEmojis) ? "/❗" :""}]`
             break;
         case 4:
-            SeverityMsg=`[MAJOR${(settings.useEmojis) ? "/❗❗" :""}]`
+            SeverityMsg=`\x1b[31m[${settings[4].keyword}${(settings.useEmojis) ? "/❗❗" :""}]`
             break;
         case 5:
-            SeverityMsg=`[CRITICAL${(settings.useEmojis) ? "/❗❗❗" :""}]`
+            SeverityMsg=`\x1b[31m[${settings[5].keyword}${(settings.useEmojis) ? "/❗❗❗" :""}]`
             break;
         default:
-            SeverityMsg=`\x1b[37m[LOG${(settings.useEmojis) ? "/📜" :""}]`
+            SeverityMsg=`\x1b[37m[${settings[0].keyword}${(settings.useEmojis) ? "/📜" :""}]`
             break;
     }
     var stack = error.stack.toString().split(/\r\n|\n/)[1].split(" ");
@@ -48,5 +48,5 @@ module.exports.print=(msg,error,severity=1,isClient=true)=>{
     if (msg === '') {
         msg = '"👨‍💻"';
     }
-    console.log(`${Context} \x1b[37m${SeverityMsg} \x1b[1m${msg} \x1b[30m${Origin}\x1b[0m`);  
+    console.log(`${Context} ${SeverityMsg}\x1b[0m \x1b[1m${msg} \x1b[30m${Origin}\x1b[0m`);  
 }
